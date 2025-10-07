d36945397c272a0749e1947d2a5369923c1ed742
Marcela Maciel <macielwebcode@gmail.com>
2025-10-07 18:40:11 -0300

    front

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -14,17 +14,37 @@ export default function Resposta(props: RespostaProps){
     return (
         <div className={styles.resposta} onClick={() => props.respostaCliente(props.indice)}>
             <div className={styles.conteudoResposta}>
-                <div className={styles.frente}>
-                    <div className={styles.letra}>
-                        {props.letra}
-                    </div>
-                    <div className={styles.valor}>
-                        {resposta.valor}
-                    </div>
-                </div>
-                <div className={styles.verso}>
+                {resposta.revelada ? (
 
-                </div>
+                    <div className={styles.frente}>
+                        <div className={styles.letra}>
+                            {props.letra}
+                        </div>
+                        <div className={styles.valor}>
+                            {resposta.valor}
+                        </div>
+                    </div>
+
+
+
+                ):(
+                    <div className={styles.verso}>
+                        {resposta.certa ? (
+                            <div className={styles.certa}>
+                                <div>A resposta correta é: </div>
+                                <div className={styles.texto}>{resposta.valor}</div>
+                            </div>
+                        ) : (
+                            <div className={styles.errada}>
+                                <div>A resposta está errada. </div>
+                                <div className={styles.texto}>{resposta.valor}</div>
+                            </div>
+                        )}
+                    
+                    </div>
+                )}  
+                
+                
             </div>
         </div>
     )
